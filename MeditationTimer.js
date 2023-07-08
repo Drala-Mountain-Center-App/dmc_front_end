534ef8fda8add7a80cfd2d71af69fa2ad9f3036d
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-07-07 19:16:01 -0700

    feat: meditation timer basic layout

---
+++ MeditationTimer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const MeditationTimer = () => {
+  return (
+    <View>
+      <Text>Meditation Timer</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default MeditationTimer;
