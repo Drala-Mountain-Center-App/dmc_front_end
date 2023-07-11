bedd6dcb24897b845c6489f2f0384b2a8fd30625
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-07-11 15:36:42 -0700

    feat: functionality for login page route with fixing bug for nested components.

--- App.js
+++ App.js
@@ -1,29 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, SafeAreaView} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import React from 'react';
-import Header from './Header';
-import HomePage from './HomePage'
-import MeditationTimer from './MeditationTimer';
-import UserLogin from './UserLogin';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, SafeAreaView } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import React from "react";
+import Header from "./Header";
+import HomePage from "./HomePage";
+import MeditationTimer from "./MeditationTimer";
+import UserLogin from "./UserLogin";
 
-const Stack = createStackNavigator()
-const AuthStack = createStackNavigator();
-
-const AuthScreens = () => {
-  return (
-    <AuthStack.Navigator>
-      <AuthStack.Screen name="UserLogin" component={UserLogin} />
-      {/* Add other authentication screens here */}
-    </AuthStack.Navigator>
-  );
-};
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <SafeAreaView style={styles.screen}>
-      {/* <Header /> */}
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen
@@ -33,16 +22,18 @@ export default function App() {
               header: () => <Header />,
             }}
           />
-          {/* <Stack.Screen name="Home Page" component={HomePage} /> */}
           <Stack.Screen
             name="20 Min Meditation Timer"
             component={MeditationTimer}
+            options={{
+              headerShown: true,
+            }}
           />
           <Stack.Screen
-            name="AuthScreens"
-            component={AuthScreens}
+            name="UserLogin"
+            component={UserLogin}
             options={{
-              headerShown: false,
+              headerShown: true,
             }}
           />
         </Stack.Navigator>
@@ -56,12 +47,5 @@ const styles = StyleSheet.create({
   screen: {
     backgroundColor: "#EAE2FA",
     flex: 1,
-  },
-  container: {
-    flex: 1,
-    backgroundColor: "#EAE2FA",
-    alignItems: "center",
-    justifyContent: "space-between",
-    width: "100%",
   },
 });
