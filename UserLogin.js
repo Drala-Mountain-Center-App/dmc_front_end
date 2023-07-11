d2b4dad8c8668ea12ded05edc8089734c9de68c8
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-07-10 19:02:29 -0700

    feat: add controlled form to user login with hooks for firstName, lastName, email, password

---
+++ UserLogin.js
@@ -0,0 +1,44 @@
+import React, {useState} from "react";
+import {View, TextInput, Button} from 'react-native'
+
+export const UserLogin = () => {
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = () => {
+        console.log({
+            firstName,
+            lastName,
+            email,
+            password
+        })
+    }
+
+    return (
+        <View>
+            <TextInput
+                placeholder="First Name"
+                value={firstName}
+                onChangeText={text => setFirstName(text)}> 
+            </TextInput>
+            <TextInput
+                placeholder="Last Name"
+                value={lastName}
+                onChangeText={text => setLastName(text)}> 
+            </TextInput>
+            <TextInput
+                placeholder="Email"
+                value={email}
+                onChangeText={email => setEmail(text)}> 
+            </TextInput>
+            <TextInput
+                placeholder="Password"
+                value={password}
+                onChangeText={text => setPassword(text)}> 
+            </TextInput>
+            <Button title="Submit" onPress={handleSubmit}></Button>
+        </View>
+    )
+}
