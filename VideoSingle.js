cbfc857ceb568e62345352b66594f01b53e0a0a7
“Sarah <sarahcatherine311@gmail.com>
2023-07-13 18:26:21 -0600

    feat: Finish the Videos page

--- VideoSingle.js
+++ VideoSingle.js
@@ -1,109 +1,39 @@
-import React, { useState, useRef } from "react";
+import React from "react";
 import WebView from "react-native-webview";
-// import {
-//   View,
-//   Text,
-//   StyleSheet,
-//   Button,
-//   ImageBackground,
-//   TouchableOpacity,
-// } from "react-native";
-// import { Video, Audio, StatusBar } from "expo-av";
+import {
+  View,
+  Text,
+  StyleSheet,
+} from "react-native";
 
-// const VideoSingle = ({ event }) => {
-//   const video = React.useRef(null);
-//   const [status, setStatus] = React.useState({});
-
-//   const formatDateString = (dateString) => {
-//     const parts = dateString.split(" ")[0].split("-");
-//     const [year, month, date] = parts;
-//     return `${month} / ${date} / ${year}`;
-//   };
-
-//   return (
-//     <View style={styles.container}>
-//       <Video
-//         ref={video}
-//         source={{ uri: event.videoUrl }}
-//         style={styles.video}
-//         resizeMode="contain"
-//         useNativeControls
-//         onPlaybackStatusUpdate={setStatus}
-//       />
-//       <View style={styles.buttons}>
-//         <Button
-//           title="Play"
-//           onPress={() => video.current.playFromPositionAsync(5000)}
-//         />
-//       </View>
-
-//       <View style={styles.buttons}>
-//         <Button
-//           title="Play from 50s"
-//           onPress={() => video.current.playFromPositionAsync(50000)}
-//         />
-//       </View>
-//       <StatusBar style="auto" />
-//       <View style={styles.infoContainer}>
-//         <Text style={styles.title}>{event.title}</Text>
-//         <Text style={styles.speaker}>{event.speaker}</Text>
-//         <Text style={styles.topic}>{event.topic}</Text>
-//         <Text style={styles.dateRecorded}>
-//           Date Recorded: {formatDateString(event.dateRecorded)}
-//         </Text>
-//       </View>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-//   video: {
-//     flex: 1,
-//     alignSelf: "stretch",
-//   },
-//   buttons: {
-//     margin: 16,
-//   },
-//   title: {
-//     fontSize: 18,
-//     fontWeight: "bold",
-//     marginBottom: 5,
-//     color: "#3d314d",
-//   },
-//   speaker: {
-//     fontSize: 16,
-//     marginBottom: 5,
-//   },
-//   topic: {
-//     fontSize: 14,
-//     marginBottom: 5,
-//   },
-//   dateRecorded: {
-//     fontSize: 12,
-//     color: "#888",
-//   },
-// });
-
+  const formatDateString = (dateString) => {
+    const parts = dateString.split(" ")[0].split("-");
+    const [year, month, date] = parts;
+    return `${month} / ${date} / ${year}`;
+  };
 
 const VideoSingle = ({ event }) => {
   return (
-    <WebView
-      style={style}
-      // onError={onError}
-      allowsFullscreenVideo
-      scrollEnabled={false}
-      automaticallyAdjustContentInsets
-      source={{
-        html: event.embedCode,
-      }}
-    />
+    <>
+      <WebView
+        style={style}
+        // onError={onError}
+        allowsFullscreenVideo
+        scrollEnabled={false}
+        automaticallyAdjustContentInsets
+        source={{
+          html: event.embedCode,
+        }}
+      />
+      <View style={styles.infoContainer}>
+        <Text style={styles.title}>{event.title}</Text>        
+        <Text style={styles.speaker}>{event.speaker}</Text>
+        <Text style={styles.topic}>{event.topic}</Text>
+        <Text style={styles.dateRecorded}>
+          Date Recorded: {formatDateString(event.dateRecorded)}
+        </Text>
+       </View>
+    </>
   );
 };
 
@@ -111,5 +41,29 @@ const style = {
   height: 200,
   maxWidth: '100%',
 };
+
+const styles = StyleSheet.create({
+    infoContainercontainer: {
+      backgroundColor: "#fff",
+    },
+    title: {
+      fontSize: 18,
+      fontWeight: "bold",
+      marginBottom: 5,
+      color: "#3d314d",
+    },
+    speaker: {
+      fontSize: 16,
+      marginBottom: 5,
+    },
+    topic: {
+      fontSize: 14,
+      marginBottom: 5,
+    },
+    dateRecorded: {
+      fontSize: 12,
+      color: "#888",
+    },
+  });
 
 export default VideoSingle;
