eb1f0dec2c8a835de07aa16bd824d9395b8a490c
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-07-15 15:17:21 -0700

    feat: update queries for video thumbnail, refactor code to open video play in another screen, basic styling.

--- src/Components/VideoSingle.js
+++ src/Components/VideoSingle.js
@@ -1,33 +1,32 @@
 import React from "react";
-import WebView from "react-native-webview";
 import {
   View,
   Text,
   StyleSheet,
+  TouchableOpacity,
+  Image,
+  Linking,
 } from "react-native";
 
-  const formatDateString = (dateString) => {
-    const parts = dateString.split(" ")[0].split("-");
-    const [year, month, date] = parts;
-    return `${month} / ${date} / ${year}`;
-  };
+const formatDateString = (dateString) => {
+  const parts = dateString.split(" ")[0].split("-");
+  const [year, month, date] = parts;
+  return `${month} / ${date} / ${year}`;
+};
 
 const VideoSingle = ({ event }) => {
+  const handleThumbnailPress = () => {
+    Linking.openURL(event.videoUrl);
+  };
+
   return (
-    <>
-      <WebView
-        style={style}
-        // onError={onError}
-        allowsFullscreenVideo
-        scrollEnabled={false}
-        automaticallyAdjustContentInsets
-        source={{
-          html: event.embedCode,
-        }}
-      />
+    <View style={styles.container}>
+      <TouchableOpacity onPress={handleThumbnailPress}>
+        <Image source={{ uri: event.thumbnailUrl }} style={styles.thumbnail} />
+      </TouchableOpacity>
       <View style={styles.infoContainer} testID="info-container">
         <Text style={styles.title} testID="event-title">
-          {event.title}{" "}
+          {event.title}
         </Text>
         <Text style={styles.speaker} testID="event-speaker">
           {event.speaker}
@@ -39,37 +38,40 @@ const VideoSingle = ({ event }) => {
           Date Recorded: {formatDateString(event.dateRecorded)}
         </Text>
       </View>
-    </>
+    </View>
   );
 };
 
-const style = {
-  height: 200,
-  maxWidth: '100%',
-};
-
 const styles = StyleSheet.create({
-    infoContainercontainer: {
-      backgroundColor: "#fff",
-    },
-    title: {
-      fontSize: 18,
-      fontWeight: "bold",
-      marginBottom: 5,
-      color: "#3d314d",
-    },
-    speaker: {
-      fontSize: 16,
-      marginBottom: 5,
-    },
-    topic: {
-      fontSize: 14,
-      marginBottom: 5,
-    },
-    dateRecorded: {
-      fontSize: 12,
-      color: "#888",
-    },
-  });
+  container: {
+    backgroundColor: "#fff",
+    marginBottom: 20,
+  },
+  thumbnail: {
+    height: 200,
+    resizeMode: "cover",
+  },
+  infoContainer: {
+    padding: 10,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 5,
+    color: "#3d314d",
+  },
+  speaker: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  topic: {
+    fontSize: 14,
+    marginBottom: 5,
+  },
+  dateRecorded: {
+    fontSize: 12,
+    color: "#888",
+  },
+});
 
 export default VideoSingle;
