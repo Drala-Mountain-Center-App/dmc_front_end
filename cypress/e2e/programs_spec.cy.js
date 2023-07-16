cafa1e0b346f2e6a69ae67e26aa325deaa775eb4
“Sarah <sarahcatherine311@gmail.com>
2023-07-16 09:46:46 -0600

    Fix cypress tests
    Co-authored-by: Samantha McElhinney <mcelhinneysamantha@gmail.com>

--- cypress/e2e/programs_spec.cy.js
+++ cypress/e2e/programs_spec.cy.js
@@ -1,19 +1,16 @@
 describe('programs spec', () => {
   beforeEach(() => {
-    // cy.intercept('POST', "https://drala-mountain-api-4812ef039e59.herokuapp.com/graphql", {
-    //   fixture: 'event-examples.json'
-    // })
     cy.visit('http://localhost:19006/')
   });
   
   it('should navigate to a new page that displays programs', () => {
-    cy.get('[data-testid="programs"]').click()
+    cy.get('.r-alignItems-1habvwh > :nth-child(2)').click()
     cy.get('[data-testid="programEvent"]')
     .should('have.length', 32)
   });
 
   it('should display the program name', () => {
-    cy.get('[data-testid="programs"]').click()
+    cy.get('.r-alignItems-1habvwh > :nth-child(2)').click()
     cy.get('[data-testid="programEvent"]').first()
     .should('contain', "Shambhala Sun Camp")
   });
